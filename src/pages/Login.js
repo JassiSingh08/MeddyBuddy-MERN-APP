2125555b93d1f54ba0e5183f0160da255c23f57e
JassiSingh08 <singhjassi693@gmail.com>
2023-05-26 15:37:52 +0530

    backend + auth token setup

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,35 +1,70 @@
-import React from 'react'
-import { useNavigate } from 'react-router-dom';
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import { Form, Input, message } from "antd";
+import axios from "axios";
+import {useDispatch} from "react-redux";
+import {showLoading, hideLoading} from "../redux/features/alertslice";
 
-const Login = () => {
-  let navigate = useNavigate()
+const Login = () => { 
+  let navigate = useNavigate();
+  const dispatch = useDispatch();
   const navtoSign = () => {
     navigate("/register");
-}
+  };
 
-  const handleSubmit = () => {
-    console.log("hello")
-  } 
+  const onfinishHandler = async (values) => {
+    try {
+      dispatch(showLoading());
+      const res = await axios.post("/api/v1/user/login", values);
+      dispatch(hideLoading());
+      if (res.data.success) {
+        localStorage.setItem("token", res.data.token);
+        message.success("Login Successfully");
+        navigate("/");
+      } else {
+        message.error(res.data.message);
+      }
+    } catch (error) {
+      dispatch(hideLoading());
+      console.log(error);
+      message.error("something went wrong");
+    }
+  };
+
   return (
-   <>
-    <div className='container my-3 mt-4'>
-            <h2>Login to continue</h2>
-            <form onSubmit={handleSubmit} >
-                <div className="mb-3 my-3">
-                    <label htmlFor="email" className="form-label">Email address</label>
-                    <input type="email" className="form-control" id="email" name='email' autoComplete="off" aria-describedby="emailHelp" />
-                </div>
-                <div className="mb-3">
-                    <label htmlFor="password" className="form-label">Password</label>
-                    <input type="password" className="form-control" id="password" autoComplete="off" name='password' />
-                </div>
+    <>
+      <div className="container my-3 mt-4">
+        <h2>Login to continue</h2>
+        <Form layout="vertical" onFinish={onfinishHandler}>
 
-                <button type="submit" className="btn btn-primary" >Let me in.</button>
-            </form>
-            <h6 className='my-4'>Not have an account? <span style={{cursor: 'pointer', textDecoration:"underline"}} onClick={navtoSign}>Create Here. </span></h6> 
-        </div>
-   </>
-  )
-}
+          <div className="mb-3 my-3">
+            <Form.Item label="Email" name="email">
+              <Input type="email" required />
+            </Form.Item>
+          </div>
 
-export default Login
+          <div className="mb-3">
+            <Form.Item label="Password" name="password">
+              <Input type="password" required />
+            </Form.Item>
+          </div>
+
+          <button type="submit" className="btn btn-primary">
+            Let me in.
+          </button>
+
+        </Form>
+        <h6 className="my-4">
+          Not have an account?
+          <span
+            style={{ cursor: "pointer", textDecoration: "underline" }}
+            onClick={navtoSign}>
+            Create Here.
+          </span>
+        </h6>
+      </div>
+    </>
+  );
+};
+
+export default Login;
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,13 +1,34 @@
 import React from "react";
+import { Form, Input, message } from "antd";
 import {useNavigate} from "react-router-dom";
+import axios from 'axios';
+import { useDispatch } from "react-redux";
+import { showLoading, hideLoading } from "../redux/features/alertslice";
 
 const Register = () => {
-  const onSubmitHandler = (values) => {
-    console.log(values);
-  };
-
-
   let navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const onfinishHandler = async(values) => {
+   try {
+        dispatch(showLoading());
+        const res = await axios.post("/api/v1/user/register", values);
+        dispatch(hideLoading());
+        if(res.data.success){
+          console.log(res.data);
+          message.success("Account created successfully");
+          navigate('/login')
+        }else{
+          message.error("Something went wrong");
+        }
+    } catch (error) {
+      dispatch(hideLoading());
+      console.log(error);
+      message.error("Something went wrong "  +error);
+    }
+  }
+    
+
   const navtoLogin = () => {
     navigate("/login");
 }
@@ -15,22 +36,24 @@ const Register = () => {
     <>
       <div className='container my-3 mt-4'>
         <h2 className='my-3 '>Create an Account to Continue</h2>
-        <form onSubmit={onSubmitHandler}>
+        <Form
+          layout="vertical"
+          onFinish={onfinishHandler}
+          className="register-form"
+        >
         <div className="mb-3">
-          <label htmlFor="name" className="form-label">Name</label>
-          <input type="text" className="form-control" id="name" name='name' aria-describedby="emailHelp" required />
-
-          <label htmlFor="email" className="form-label">Email address</label>
-          <input type="email" className="form-control" id="email" name='email' aria-describedby="emailHelp" required autoComplete="off"/>
-
-          <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
-        </div>
-        <div className="mb-3">
-          <label htmlFor="password" className="form-label">Password</label>
-          <input type="password" className="form-control" id="password" name='password' required minLength={5} autoComplete="off"/>
+        <Form.Item label="Name" name="name">
+            <Input type="text" required />
+          </Form.Item>
+          <Form.Item label="Email" name="email">
+            <Input type="email" required />
+          </Form.Item>
+          <Form.Item label="Password" name="password">
+            <Input type="password" required />
+          </Form.Item>
         </div>
         <button type="submit" className="btn btn-primary"  >Create Account</button>
-      </form>
+      </Form>
       <h6 className='my-4'>Already have an Account? <span style={{cursor: 'pointer', textDecoration:"underline"}} onClick={navtoLogin}>Login Here. </span></h6> 
     </div>
     </>
