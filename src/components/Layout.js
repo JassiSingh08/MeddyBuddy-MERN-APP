f16e65b44411a81f6f1190296a9e2b196d0a631d
JassiSingh08 <singhjassi693@gmail.com>
2023-05-28 23:49:21 +0530

    sidebar Implementation

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,21 +1,62 @@
 import React from "react";
+import { Link, useLocation } from "react-router-dom";
 
 const Layout = ({ children }) => {
+
+  const location = useLocation();
+  const SidebarMenu = [
+    {
+      name: "Home",
+      path: "/",
+      icon: "fa-solid fa-house",
+    },
+    {
+      name: "Appointments",
+      path: "/appointments",
+      icon: "fa-solid fa-list",
+    },
+    {
+      name: "Apply Doctor",
+      path: "/apply-doctor",
+      icon: "fa-solid fa-user-doctor",
+    },
+    {
+      name: "Profile",
+      path: "/profile",
+      icon: "fa-solid fa-user",
+    },
+    {
+      name: "Logout",
+      path: "/logout",
+      icon: "fa-solid fa-right-from-bracket",
+    },
+  ];
   return (
     <>
       <div className="main">
         <div className="layout">
           <div className="sidebar">
-            logo 
-            Menu Items 
+            <div className="logo">
+              <h6>Meddy Buddy</h6>
+              <hr />
+            </div>
+            <div className="menu">
+              {SidebarMenu.map((menu) => {
+                const isActive = location.pathname === menu.path;
+                return (
+                  <>
+                    <div className={`menu-item ${isActive && "active"}`}>
+                      <i className={menu.icon}></i>
+                      <Link to={menu.path}>{menu.name}</Link>
+                    </div>
+                  </>
+                );
+              })}
+            </div>
           </div>
-          <div className="leftside">
-            <div className="header">
-                user profile , etc 
-            </div>
-            <div className="body">
-                appoint cards
-            </div>
+          <div className="content">
+            <div className="header">Header</div>
+            <div className="body">{children}</div>
           </div>
         </div>
       </div>
